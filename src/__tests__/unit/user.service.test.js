8dd0b5a09b8025a64055633030d9c9a389620380
= <=>
2025-05-22 15:39:15 +0100

    adds sample test

---
+++ src/__tests__/unit/user.service.test.js
@@ -0,0 +1,12 @@
+/* eslint-env jest */
+/* global , describe, it, expect */
+
+
+describe('createUser', () => {
+  
+ 
+
+  it('should return true', async () => {
+    expect(true).toBe(true);
+  });
+});
